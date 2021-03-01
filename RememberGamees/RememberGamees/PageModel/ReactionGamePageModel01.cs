a0334e6ce197481023e3575cd2a72d669c92ccea
Pavol Hodás <pavolhodas4@gmail.com>
2021-03-01 10:08:02 +0100

    set grid add image and 3x imageButton

--- RememberGamees/RememberGamees/PageModel/ReactionGamePageModel01.cs
+++ RememberGamees/RememberGamees/PageModel/ReactionGamePageModel01.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace RememberGamees.PageModel
 {
     class ReactionGamePageModel01
     {
+
+        public Command firstBtn_Clicked => new Command(() =>
+        {
+        });
+
+        public Command secondBtn_Clicked => new Command(() =>
+        {
+
+        });
+
+        public Command thirdBtn_Clicked => new Command(() =>
+        {
+
+        });
+
+        public object VariableImage { get; private set; }
     }
 }
