17c418272a617e16e9ec332b3c87bc20890ac2ef
Pavol Hodás <pavolhodas4@gmail.com>
2021-03-03 19:51:01 +0100

    Add random image after button clicked

--- RememberGamees/RememberGamees/Pages/ReactionGamePage01.xaml.cs
+++ RememberGamees/RememberGamees/Pages/ReactionGamePage01.xaml.cs
@@ -13,10 +13,21 @@ namespace RememberGamees.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ReactionGamePage01 : ContentPage
     {
+        List<string> images = new List<string> { "Image1", "Image2", "Image3", "Image4", "Image5", "Image6", "Image7", "Image8", "Image8", "Image9", "Image10" };
+
         public ReactionGamePage01()
         {
+
             InitializeComponent();
-            BindingContext = new ReactionGamePageModel01();
+            //BindingContext = new ReactionGamePageModel01();
+        }
+        private void thirdBtn_Clicked(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            var next = rand.Next(10);
+            var image = images[next];
+
+            RandomImg.Source = ImageSource.FromFile(image);
         }
     }
 }
