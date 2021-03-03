a0c4ce6481772467dcbae277976385c84c9c9a9e
Timotej Panták <timopantakgit@gmail.com>
2021-03-03 13:39:34 +0100

    Add score popup method

--- RememberGamees/RememberGamees/PageModel/ReactionGamePageModel01.cs
+++ RememberGamees/RememberGamees/PageModel/ReactionGamePageModel01.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using Xamarin.Forms;
+using System.Timers;
+
 
 namespace RememberGamees.PageModel
 {
-    class ReactionGamePageModel01
+    class ReactionGamePageModel01 
     {
 
         public Command firstBtn_Clicked => new Command(() =>
@@ -19,9 +22,26 @@ namespace RememberGamees.PageModel
 
         public Command thirdBtn_Clicked => new Command(() =>
         {
-
+            
         });
 
         public object VariableImage { get; private set; }
+
+
+        
+        
+       
+        
+        //popup method
+        public static async void PopUp()
+        {
+            await App.Current.MainPage.DisplayAlert("Score", "You reached:(Score)", "OK");
+        }
+
+        //timer
+        public void SecTimer()
+        { 
+
+        }
     }
 }
