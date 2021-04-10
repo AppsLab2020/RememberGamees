5fc03aeb7357d6d6ef6ce4dc51f03f8492e3a28d
pavolhodas <pavolhodas4@gmail.com>
2021-04-10 22:20:52 +0200

    Add the base for saving to SQLite

---
+++ RememberGamees/RememberGamees/Model/Experiences.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RememberGamees.Model
+{
+    class Experiences
+    {
+        public Experiences()
+        {
+            ReadAt = DateTime.Now;
+        }
+        [PrimaryKey, AutoIncrement]
+        public int ExperienceNumber { get; set; }
+        public DateTime ReadAt { get; set; }
+    }
+}
---
+++ RememberGamees/RememberGamees/Model/SQLiteExperiences.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RememberGamees.Model
+{
+    public class SQLiteExperiences
+    {
+        private SQLiteAsyncConnection _connection;
+
+        public async Task Initialize()
+        {
+            if (_connection != null) return;
+
+            _connection = new SQLiteAsyncConnection(
+                Path.Combine(Xamarin.Essentials.FileSystem.AppDataDirectory, "experiences.db3"));
+
+            await _connection.CreateTableAsync<Experiences>();
+        }
+    }
+}
