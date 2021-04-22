b5c1befb72afe3f9637c389046a5971c0d17f79d
pavolhodas <pavolhodas4@gmail.com>
2021-04-22 16:56:41 +0200

    Add Person object, list of it and save it to specialfolder to SQLite(db3 format)

--- RememberGamees/RememberGamees/App.xaml.cs
+++ RememberGamees/RememberGamees/App.xaml.cs
@@ -1,19 +1,49 @@
-﻿using RememberGamees;
+﻿using RememberGamees.Data;
 using RememberGamees.Pages;
 using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace RememberGamees
 {
     public partial class App : Application
     {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
+
             Device.SetFlags(new[] { "Brush_Experimental" });
             //MainPage = new ShellApp();
             MainPage = new Page1();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
         }
     }
 }
---
+++ RememberGamees/RememberGamees/Data/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RememberGamees.Data
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Experiences { get; set; }
+    }
+}
