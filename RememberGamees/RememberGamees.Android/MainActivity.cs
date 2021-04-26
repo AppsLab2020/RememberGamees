292a5c9294e08e6cfd98c21ff240e26308de560c
pavolhodas <pavolhodas4@gmail.com>
2021-04-26 09:06:25 +0200

    Add interface

--- RememberGamees/RememberGamees.Android/MainActivity.cs
+++ RememberGamees/RememberGamees.Android/MainActivity.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Firebase;
 
 namespace RememberGamees.Droid
 {
@@ -19,6 +19,7 @@ namespace RememberGamees.Droid
 
             base.OnCreate(savedInstanceState);
 
+            FirebaseApp.InitializeApp(Application.Context);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
