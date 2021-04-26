8a004569b6602f10a1d269389e264939b8ebb78a
pavolhodas <pavolhodas4@gmail.com>
2021-04-26 14:58:13 +0200

    Create random Image after one of buttons clicked

--- RememberGamees/RememberGamees/PageModel/MemoryGamePageModel.cs
+++ RememberGamees/RememberGamees/PageModel/MemoryGamePageModel.cs
@@ -8,12 +8,19 @@ namespace RememberGamees.PageModel
 {
     class MemoryGamePageModel : INotifyPropertyChanged
     {
+        List<string> images = new List<string> { "Image1", "Image2", "Image3", "Image4", "Image5", "Image6", "Image7", "Image8", "Image8", "Image9" };
+        private string randomImage;
+
         private ImageSource firstImage = "Image1";
         private ImageSource secondImage = "Image2";
         private ImageSource thirdImage = "Image3";
         private ImageSource fourthImage = "Image4";
         private ImageSource fivethImage = "Image5";
         private ImageSource sixthImage = "Image6";
+        private ImageSource oldImage;
+
+        private int AdditionExperience;
+        private int fifty;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -30,17 +37,24 @@ namespace RememberGamees.PageModel
 
         public Command Yes_Clicked => new Command(async () =>
         {
-
+            CreateRandomImage();
         });
 
         public Command No_Clicked => new Command(async () =>
         {
-
+            CreateRandomImage();
         });
 
         public MemoryGamePageModel()
         {
 
+        }
+        private void CreateRandomImage()
+        {
+            var rand = new Random();
+            var next = rand.Next(9);
+            randomImage = images[next];
+            RandomI = ImageSource.FromFile(randomImage);
         }
     }
 }
