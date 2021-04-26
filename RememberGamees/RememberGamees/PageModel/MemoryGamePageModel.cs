820d29a4ef30698bd405f6950beceb392d95cecb
pavolhodas <pavolhodas4@gmail.com>
2021-04-26 19:04:09 +0200

    Run a second game without database

--- RememberGamees/RememberGamees/PageModel/MemoryGamePageModel.cs
+++ RememberGamees/RememberGamees/PageModel/MemoryGamePageModel.cs
@@ -8,8 +8,10 @@ namespace RememberGamees.PageModel
 {
     class MemoryGamePageModel : INotifyPropertyChanged
     {
-        List<string> images = new List<string> { "Image1", "Image2", "Image3", "Image4", "Image5", "Image6", "Image7", "Image8", "Image8", "Image9" };
+        List<string> images = new List<string> { "Image1", "Image2", "Image3", "Image4", "Image5", "Image6", "Image7"};
         private string randomImage;
+        private string newRandomImage { get; set; }
+        private int nullExperience;
 
         private ImageSource firstImage = "Image1";
         private ImageSource secondImage = "Image2";
@@ -17,10 +19,11 @@ namespace RememberGamees.PageModel
         private ImageSource fourthImage = "Image4";
         private ImageSource fivethImage = "Image5";
         private ImageSource sixthImage = "Image6";
-        private ImageSource oldImage;
 
         private int AdditionExperience;
         private int fifty;
+
+        private string setExperience;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -34,14 +37,62 @@ namespace RememberGamees.PageModel
                 .Invoke(this, new PropertyChangedEventArgs(nameof(RandomI)));
             }
         }
-
-        public Command Yes_Clicked => new Command(async () =>
+        public string randNumb
         {
+            get => randomImage;
+            set
+            {
+                firstImage = value;
+                PropertyChanged?
+                .Invoke(this, new PropertyChangedEventArgs(nameof(randNumb)));
+            }
+        }
+
+        public string NewestRandNumb
+        {
+            get => newRandomImage;
+            set
+            {
+                firstImage = value;
+                PropertyChanged?
+                .Invoke(this, new PropertyChangedEventArgs(nameof(NewestRandNumb)));
+            }
+        }
+
+        public string Experiences2
+        {
+            get => setExperience;
+            set
+            {
+                setExperience = value;
+                PropertyChanged?
+                .Invoke(this, new PropertyChangedEventArgs(nameof(Experiences2)));
+            }
+        }
+
+        public Command Yes_Clicked => new Command( () =>
+        {
+            if (NewestRandNumb == randNumb)
+            {
+                fifty = AdditionExperience + 50;
+                AdditionExperience = fifty;
+                Experiences2 = AdditionExperience.ToString();
+            }
+
+            newRandomImage = randomImage;
             CreateRandomImage();
         });
 
-        public Command No_Clicked => new Command(async () =>
+        public Command No_Clicked => new Command( () =>
         {
+            if (NewestRandNumb != randNumb)
+            {
+                fifty = AdditionExperience + 50;
+                AdditionExperience = fifty;
+                Experiences2 = AdditionExperience.ToString();
+            }
+
+            newRandomImage = randomImage;
             CreateRandomImage();
         });
 
@@ -52,9 +103,14 @@ namespace RememberGamees.PageModel
         private void CreateRandomImage()
         {
             var rand = new Random();
-            var next = rand.Next(9);
+            var next = rand.Next(7);
             randomImage = images[next];
             RandomI = ImageSource.FromFile(randomImage);
+            async void WriteRandString()
+            {
+            randNumb = next.ToString();
+            }
+           
         }
     }
 }
