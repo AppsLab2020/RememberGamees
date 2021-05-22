a3cbed54e3eb009ab7c49d254b5aeb71530ec31e
pavolhodas <pavolhodas4@gmail.com>
2021-05-22 19:18:09 +0200

    Add navigation from ScoreOfMath page  to root page

---
+++ RememberGamees/RememberGamees/PageModel/ScoreMathPageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RememberGamees.PageModel
+{
+    public class ScoreMathPageModel
+    {
+        public ScoreMathPageModel(INavigation navigation)
+        {
+            RootButton = new Command(async () => await navigation.PopToRootAsync());
+        }
+        public INavigation navigation { get; set; }
+        public Command RootButton { get; set; }
+    }
+}
