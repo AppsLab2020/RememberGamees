035e28a22e677dae53af45abc87b9051fecaf91c
pavolhodas <pavolhodas4@gmail.com>
2021-06-23 19:32:34 +0200

    Add basic graphic to Tic Tac Toe

---
+++ RememberGamees/RememberGamees/PageModel/LogicalGamePageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace RememberGamees.PageModel
+{
+    public class LogicalGamePageModel : INotifyPropertyChanged
+    {
+        
+        public LogicalGamePageModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
